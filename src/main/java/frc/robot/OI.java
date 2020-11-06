94581cb518b1bd2ff5cad955c738009f76b34308
sharonepstein <sharonpalchik@gmail.com>
2020-11-06 11:44:06 +0200

    This is my robot of Sharon Epstein.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,12 +3,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.Feed;
 import frc.robot.commands.Grip;
-import frc.robot.commands.MoveRail;
-import frc.robot.commands.ShootPID;
-import frc.robot.commands.UnGrip;
+
+
+import frc.robot.subsystems.Fedeer;
 import frc.robot.subsystems.Gripper;
-import frc.robot.subsystems.Rail;
 import frc.robot.subsystems.Shooter;
 
 
@@ -20,12 +20,12 @@ public class OI {
     private JoystickButton gripButton = new JoystickButton(joystickLeft, 3);
     private JoystickButton shootButton = new JoystickButton(joystickLeft, 4);
 
-    public OI(Gripper gripper, Rail rail, Shooter shooter){
-        gripButton.whenPressed(new Grip(gripper).andThen(new MoveRail(rail),
-                new InstantCommand(rail::incrementCurrentCapacity, rail)));
-        shootButton.whenPressed(new MoveRail(rail).andThen(new ShootPID(shooter, Shooter.KP,
+    public OI(Gripper gripper, Fedeer fedeer, Shooter shooter){
+        gripButton.whenPressed(new Grip(gripper).andThen(new Feed(fedeer),
+                new InstantCommand(fedeer::incrementCurrentCapacity, fedeer)));
+        shootButton.whenPressed(new Feed(fedeer).andThen(new ShootPID(shooter, Shooter.KP,
                         Shooter.KI, Shooter.KD, Shooter.TOLERANCE, Shooter.SETPOINT),
-                new InstantCommand(rail::decrementCurrentCapacity, rail)));
+                new InstantCommand(fedeer::decrementCurrentCapacity, fedeer)));
     }
 
     public double getLeftX() {
